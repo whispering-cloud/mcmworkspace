f3a1257c00defb75564d98b401f9e7194b6695b5
Jesse Senior <jessesenior@outlook.com>
2022-02-21 06:43:12 +0800

    Update: 2022_MCM_ICM/solution/sol.cpp

--- 2022_MCM_ICM/solution/sol.cpp
+++ 2022_MCM_ICM/solution/sol.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdio>
 #include <cstring>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -16,33 +17,22 @@ struct status
 const double c_r = 0.003, g = 9.8, eta = 0.95, rho = 1.2, c_wA = 0.31;
 const int m = 80, R = 1000;
 
-double h[10001];
-int cnt;
-double v_w[10000];
-
-const int INF = 0x3f3f3f3f;
-status ansr;
-short ansp[1460];
-int total_time;
-
+double h[10001], v_w[10000];
 double M, E, CP, LT;
-static int dp[1461][1001][301];
+int cnt, total_time;
+int dp[1461][1001][301], ansp[1460];
+
+status result;
 vector<status> pre[1001][301];
-/*
-tuple<int, int, int> delta(int s, int p, int v) // r,s,v
-{
-		double theta = atan2((h[s + 1] - h[s]), 0.01);
-		double f = c_r * m * g + rho * c_wA * (v / 10 - v_w[s]) * (v / 10 - v_w[s]) + g * sin(theta) * m;
-		double a = (-f * v / 10 + p) / (v / 10 * m);
-		return make_tuple(CP - double(p) / E, (2 * v + int(10 * a)) / 2, (int)(10 * a));
-}*/
 
 status delta(int s, int p, int v)
 {
 	status res;
+	if (v / 10 * m == 0)
+		return {0, 0, 0, 0};
 	double theta = atan((h[s + 1] - h[s]) * 100);
 	double f = c_r * m * g + rho * c_wA * (v / 10 - v_w[s]) * (v / 10 - v_w[s]) + g * sin(theta) * m;
-	double a = v / 10 * m != 0 ? (-f * v / 10 + p) / (v / 10 * m) : 0.1;
+	double a = (-f * v / 10 + p) / (v / 10 * m);
 	res.dv = int(10 * a);
 	res.ds = v / 10;
 	res.dr = 10 * (CP - double(p) / E);
@@ -51,25 +41,41 @@ status delta(int s, int p, int v)
 // 161.65 1.45 0.038 0.24
 int solve()
 {
-	status tmpt, del;
-	for (int i = 0; i < 1460; i++)
+	status del;
+
+	int pmax = M;
+	for (int p = 0; p <= pmax; p++)
 	{
-		// bool debugflag = false;
-		cout << i << endl;
-		for (int j = 1000; j >= 0; j--)
+		del = delta(0, p, 1);
+		if (del.ds < 0)
+			continue;
+		int tmpr = max(0, min(1000, 1000 + del.dr));
+		int tmpv = max(0, min(300, del.dv));
+		int tmps = del.ds;
+
+		if (dp[1][tmpr][tmpv] < tmps)
 		{
-			// bool flag = false;
+			dp[1][tmpr][tmpv] = tmps;
+			while (pre[tmpr][tmpv].size() <= 1)
+				pre[tmpr][tmpv].push_back({-1, -1, -1, -1});
+			pre[tmpr][tmpv][1] = {1000, 0, 1, p};
+		}
+	}
+
+	for (int i = 1; i < 1460; i++)
+	{
+		if (i % 10 == 0)
+			cout << i << endl;
+		for (int j = 1000; j >= 0; j--)
 			for (int k = 0; k <= 300; k++)
 			{
-				if (dp[i][j][k] >= INF)
+				if (pre[j][k].size() <= i || pre[j][k][i].ds==-1)
 					continue;
-				// else
-				//	debugflag = true;
-				if (dp[i][j][k] >= cnt && dp[i][j][k] < INF)
+				if (dp[i][j][k] >= cnt/10)
 				{
-					ansr.ds = dp[i][j][k];
-					ansr.dr = j;
-					ansr.dv = k;
+					result.ds = dp[i][j][k];
+					result.dr = j;
+					result.dv = k;
 					return i;
 				}
 				int pmax = M * j / 1000 - LT * i;
@@ -80,28 +86,17 @@ int solve()
 						continue;
 					int tmpr = max(0, min(1000, j + del.dr));
 					int tmpv = max(0, min(300, k + del.dv));
-					int tmps = max(0, dp[i][j][k] + del.ds);
+					int tmps = dp[i][j][k] + del.ds;
 
-					int judger = dp[i + 1][tmpr][tmpv] >= INF ? -1 : dp[i + 1][tmpr][tmpv];
-
-					if (judger < tmps)
+					if (dp[i + 1][tmpr][tmpv] < tmps)
 					{
 						dp[i + 1][tmpr][tmpv] = tmps;
-						tmpt.ds = i; // this ds = time
-						tmpt.dr = j;
-						tmpt.dv = k;
-						tmpt.power = p;
-						if (i == 0)
-							continue;
 						while (pre[tmpr][tmpv].size() <= i + 1)
 							pre[tmpr][tmpv].push_back({-1, -1, -1, -1});
-						pre[tmpr][tmpv][i] = tmpt;
+						pre[tmpr][tmpv][i + 1] = {j, i, k, p};
 					}
 				}
 			}
-		}
-		// if (!debugflag)
-		//	system("pause");
 	}
 
 	return -1;
@@ -109,44 +104,38 @@ int solve()
 
 void path()
 {
-	int j = ansr.dr;
-	int k = ansr.dv;
-	for (int i = total_time - 1; i > 0; i--)
+	int j = result.dr;
+	int k = result.dv;
+	for (int i = total_time; i > 0; i--)
 	{
-		if(i==252) {
-			cout << "Attention";
-		}
-		ansp[i] = pre[j][k][i].power;
-		j = pre[j][k][i].dr;
-		k = pre[j][k][i].dv;
+		ansp[i - 1] = pre[j][k][i].power;
+		int tj = pre[j][k][i].dr, tk = pre[j][k][i].dv;
+		j = tj;
+		k = tk;
 	}
 }
 
 int main()
 {
-	cout << "Input cyclers' M E CP LT: ";
+	string terrain_src_path;
+	cout << "Input source of terrain data: " << endl;
+	terrain_src_path = "ICM_MCM/men-elite_data.txt";
+	{
+		ifstream terrain(terrain_src_path.data());
+		while (terrain >> h[cnt++])
+			h[cnt-1]*=0.1;
+		h[cnt] = h[cnt - 1];
+	}
+	cout << "Input cyclers' M E CP LT: " << endl;
 	M = 161.65;
 	E = 1.45;
 	CP = 0.38;
 	LT = 0.24;
 	// cin >> M >> E >> CP >> LT;
-	string terrain_src;
-	cout << "Input source of terrain data" << endl;
-	terrain_src = "ICM_MCM/men-elite_data.txt";
-	freopen(terrain_src.data(), "r", stdin);
-	while (cin >> h[cnt++])
-		;
-	h[cnt] = h[cnt - 1];
-	memset(dp, INF, sizeof(dp));
-	for (int i = 0; i <= 1000; i++)
-		dp[0][i][0] = 0;
-	total_time=solve();
-	cout << total_time;
+	total_time = solve();
+	cout << "Found: " << total_time << endl;
 	path();
-	for (int i = 1; i < total_time; i++)
-	{
+	for (int i = 0; i < total_time - 1; i++)
 		cout << ansp[i] << ' ';
-	}
-	// freopen(".out","w",stdout);
 	return 0;
 }
