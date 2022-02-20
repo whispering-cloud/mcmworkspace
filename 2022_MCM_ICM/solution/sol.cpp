215f003e3f14addfbe8591a314e81c50eac693ce
whispering-cloud <baobao_wu1@sina.com>
2022-02-20 18:59:07 +0800

    soluuuu
    qwq

--- 2022_MCM_ICM/solution/sol.cpp
+++ 2022_MCM_ICM/solution/sol.cpp
@@ -28,28 +28,36 @@ double v_w[10000];
 status ansr;
 short ansp[1460];
 
-int M,E,CP;
+int M,E,CP,LT;
 status *tmmp;
 static short dp[1460][1002][301];
 vector<status> pre[1002][301];
+/*
+tuple<int, int, int> delta(int s, int p, int v) // r,s,v
+{
+		double theta = atan2((h[s + 1] - h[s]), 0.01);
+		double f = c_r * m * g + rho * c_wA * (v / 10 - v_w[s]) * (v / 10 - v_w[s]) + g * sin(theta) * m;
+		double a = (-f * v / 10 + p) / (v / 10 * m);
+		return make_tuple(CP - double(p) / E, (2 * v + int(10 * a)) / 2, (int)(10 * a));
+}*/
 
 status delta(int s,int p,int v){
 	status res;
-	double theta = atan2((h[s * 100 + 1] - h[s * 100]) , 0.01);
+	double theta = atan2((h[s + 1] - h[s]) , 0.01);
 	double f = c_r * m * g + rho * c_wA * (v / 10 - v_w[s]) * (v / 10 - v_w[s]) + g * sin(theta) * m;
-	double a = (-f * v / 10 + p) / (v / 10 * m + 1);
+	double a = (-f * v / 10 + p) / (v / 10 * m + 0.01);
 	res.dv = int(100*a);
 	res.ds = (2*v+int(100*a))/2;
-	res.dr = CP - double(p) / E;
+	res.dr = 10 * (CP - double(p) / E);
 	return res;
 }
 
 int solve(){
-	int i=0,j=100,k=0;
+	cout<<1;
 	status tmpt,del;
 	for (int i=0;i<1460;i++){
 		//cout<<i<<endl;
-		for (int j=100;j>=0;j--){
+		for (int j=1000;j>=0;j--){
 			for (int k=0;k<=250;k++){
 				if(dp[i][j][k]>=cnt) {
 					//cout<<dp[i][j][k];
@@ -59,10 +67,10 @@ int solve(){
 					return i;
 				}
 				if(dp[i][j][k]==-1) continue;
-				int pmax=M*j/100;
+				int pmax=M*j/1000-LT*i;
 				for (int p=10;p<=pmax;p+=10){
 					del = delta(dp[i][j][k],p,k);
-					int tmpr=max(0,min(100,j+del.dr));
+					int tmpr=max(0,min(1000,j+del.dr));
 					if(dp[i+1][tmpr][k+del.dv]<dp[i][j][k]+del.ds){
 						tmpt.ds=i;//this ds = time
 						tmpt.dr=j;
@@ -104,9 +112,9 @@ int main()
 	freopen(terrain_src.data(),"r",stdin);
 	while (cin>>h[cnt++]);
 	freopen("CON","r",stdin);
-	cout<<"Input cyclers M E CP\n";
+	cout<<"Input cyclers M E CP LT\n";
 	//cin>>M>>E>>CP;
-	scanf("%d %d %d",&M,&E,&CP);
+	scanf("%d %d %d",&M,&E,&CP,&LT);
 	ms(dp,-1);
 	for (int i=0;i<=100;i++){
 		dp[0][i][0]=0;
